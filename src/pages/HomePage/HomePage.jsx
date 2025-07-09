7741339071600ebe0c9db14fb4ee7f58583719bb
Gaitan Matias <gaitanmatiasg@gmail.com>
2025-07-09 01:37:14 -0300

    feat(HomePage): Modifica HomePage para renderizar criptomonedas de prueba con mockData y agrega estilos responsivos

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,9 +1,23 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import './HomePage.css'
+import mockCryptos from '../../mockData'
+import CryptoList from '../../components/CryptoListComponent/CryptoListComponent'
 
 function HomePage() {
   return (
-    <main>
-      <h1>HomePage!</h1>
+    <main className="home-page">
+      <section className="hero">
+        <h1 className='hero-title'>Sigue tus criptos favoritas en tiempo real</h1>
+        <p className="slogan">Tu puerta de entrada al mundo de las criptomonedas. Consulta precios en tiempo real, analiza tendencias y administra tu lista de seguimiento fácilmente.</p>
+      </section>
+
+      <section className='crypto-section'>
+          {/* <SearchBar /> */}
+        <section className='crypto-list-section'>
+          <p className='crypto-list-description'>Explora las criptomonedas más populares</p>
+          <CryptoList cryptos={mockCryptos} />
+        </section>
+      </section>
     </main>
   )
 }
