863e0bbc5a9e2056e93e24f5429a131d6640d5fa
Gaitan Matias <gaitanmatiasg@gmail.com>
2025-07-09 20:52:00 -0300

    chore: crea icons.js para centralizar y gestionar los íconos de react-icons

---
+++ src/icons/icons.js
@@ -0,0 +1,9 @@
+import { HiOutlineSearch } from "react-icons/hi";
+import { HiXMark } from "react-icons/hi2";
+
+const Icons = {
+  Search: HiOutlineSearch,
+  Close: HiXMark,
+};
+
+export default Icons;
