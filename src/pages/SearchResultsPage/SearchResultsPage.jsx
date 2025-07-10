44e31f585f2a844b684009f320af297aac4dcb8d
Gaitan Matias <gaitanmatiasg@gmail.com>
2025-07-10 03:17:23 -0300

    feat(SearchResultsPage): añade pagina de resultados SearchResultsPage que renderiza los resultados de busqueda con estilo responsivo

---
+++ src/pages/SearchResultsPage/SearchResultsPage.jsx
@@ -0,0 +1,65 @@
+import { useParams } from "react-router-dom";
+import { useEffect, useState } from "react";
+import CryptoList from "../../components/CryptoListComponent/CryptoListComponent";
+import "./SearchResultsPage.css";
+
+const SearchResults = () => {
+  const { searchTerm } = useParams();
+  const [results, setResults] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchSearchResults = async () => {
+      try {
+        const res = await fetch(`https://api.coingecko.com/api/v3/search?query=${searchTerm}`);
+        const data = await res.json();
+
+        const ids = data.coins.map((coin) => coin.id).join(",");
+        if (!ids) {
+          setResults([]);
+          return;
+        }
+
+        const marketRes = await fetch(
+          `https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&ids=${ids}`
+        );
+        const marketData = await marketRes.json();
+        setResults(marketData);
+      } catch (error) {
+        console.error("Error al buscar:", error);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchSearchResults();
+  }, [searchTerm]);
+
+  return (
+    <main className="search-results">
+      <div className="search-results__container">
+        <section className="search-results__header">
+          <h1 className="search-results__header-title">Buscaste: 
+            <span className="search-results__header-highlight">
+              "{searchTerm.toLocaleUpperCase()}"
+            </span>
+          </h1>
+          <p className="search-results__header-description">Mostramos los resultados más relevantes relacionados con tu búsqueda.</p>
+        </section>
+        
+        {loading ? (
+          <p className="search-results__loading">Cargando...</p>
+        ) : results.length === 0 ? (
+          <p className="search-results__empty">No se encontraron resultados.</p>
+        ) : (
+          <section className="search-results__body">
+            <h2 className="search-results__body-title">Resultados encontrados ({results.length})</h2>
+            <CryptoList cryptos={results} />
+          </section>
+        )}
+      </div>
+    </main>
+  );
+};
+
+export default SearchResults;
